99adedb9677b476b76b1fecfb5034b5433c4b17e
FriendlyGhost <ffriendlygghostt@gmail.com>
2024-12-08 02:05:40 +0500

    test

--- Program.cs
+++ Program.cs
@@ -1,3 +1,23 @@
-﻿Console.WriteLine("FirstString");
+﻿Console.WriteLine("1 Строка - мейн, неконфликт");
+Console.WriteLine("2 Строка - мейн, неконфликт");
+Console.WriteLine("3 Строка - мейн, неконфликт");
 
-Console.WriteLine("FirstString"); Console.WriteLine("FirstString");
+Console.WriteLine("5 строка мейн, конфликт");
+Console.WriteLine("6 строка мейн - конфликт");
+Console.WriteLine("7 строка мейн - не конфликт");
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine("21 Строка - мейн, неконфликт");
+Console.WriteLine("22 Строка - мейн, неконфликт");
+Console.WriteLine("23 Строка - мейн, неконфликт");
